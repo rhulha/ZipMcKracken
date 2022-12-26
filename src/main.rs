6670e347bcfee85f04cf6f20fec84f41ae376123
Ray <java4life@gmail.com>
2022-12-26 05:42:43 +0100

    So I wanted to make the program multi-threaded. But I ran into the famous "Borrowed value does not live long enough when used by thread" problem. So I tried using crossbeam threads. But they don't run in parralel. They wait... https://stackoverflow.com/questions/72090863/borrowed-value-does-not-live-long-enough-when-used-by-thread
    So I went back to using normal threads but this time I used Box::leak to make the data static.
    I this commit I keep the crossbeam stuff for posterity.
    In the next commit I will remove it.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,24 @@
 //use std::io::Read;
 use flate2::write::DeflateDecoder;
 use std::io::Write;
+use std::thread;
+//use crossbeam_utils::thread;
+use std::process;
+use chrono;
+use std::time::Instant;
+use std::env;
+use lazy_static::lazy_static;
 
 mod crctable;
 mod ziputils;
+
+const NUM_THREADS: u8 = 14;
+
+lazy_static! {
+    static ref NOW : Instant = Instant::now();
+}
+
+
 
 fn crc(crc: u32, input: u8) -> u32 {
     return (crc >> 8) ^ crctable::CRCTABLE[((crc & 0xff) as u8 ^ input) as usize];
@@ -14,7 +29,7 @@ fn update_keys(keys: &mut [u32; 3], c: u8) {
     keys[0] = crc(keys[0], c);
     keys[1] = keys[1].wrapping_add(keys[0] & 0xff);
     // keys[1] = keys[1] * 134775813 + 1;
-    keys[1] = keys[1].wrapping_mul(134775813) + 1;
+    keys[1] = keys[1].wrapping_mul(134775813).wrapping_add(1);
     keys[2] = crc(keys[2], (keys[1] >> 24) as u8);
 }
 
@@ -93,15 +108,6 @@ fn try2decrypt(data: &[u8], password: &[u8], crc: u32) -> bool {
 
 const VALID_LETTERS: &[u8;62] = b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-fn get_password_string(password : &[u8]) -> String {
-    let mut password_chars : Vec<u8> = Vec::new();
-    for i in 0..password.len() {
-        password_chars.push(VALID_LETTERS[password[i] as usize]);
-    }
-    let s = std::str::from_utf8(&password_chars).unwrap();
-    return s.to_string();
-}
-
 fn get_password_char_array(password : &[u8]) -> Vec<u8> {
     let mut password_chars : Vec<u8> = Vec::new();
     for i in 0..password.len() {
@@ -110,32 +116,11 @@ fn get_password_char_array(password : &[u8]) -> Vec<u8> {
     return password_chars
 }
 
-fn main() {
-    println!("ZipMcKracken running...");
+fn get_password_string(password : &[u8]) -> String {
+    return std::str::from_utf8(&get_password_char_array(password)).unwrap().to_string();
+}
 
-    let data = std::fs::read("test.zip").unwrap();
-
-    // let s = std::mem::size_of::<ZipFileRecord>();
-    // println!("size_of ZipFileRecord: {}", s);
-    // wrong due to allignment issue
-
-    let zfr = ziputils::read_zip_file_record( &data );
-
-    ziputils::print_zip_file_record(&zfr);
-
-    let zip_file_record_size = 30;
-    let filename_end = zip_file_record_size + zfr.filename_length as usize;
-    let filename = &data[zip_file_record_size..filename_end];
-
-    let filename_str = std::str::from_utf8(&filename).unwrap();
-    println!("filename: {:?}", filename_str);
-
-    let extra = &data[filename_end..(filename_end + zfr.extra_field_length as usize)];
-
-    let start_of_data = filename_end + zfr.extra_field_length as usize;
-    
-    println!("extra: {:?}", extra);
-
+fn main2(thread_nr: u8, compressed_data: &[u8], crc: u32) {
     let mut counter: u64 = 0;
 
     for pw_len in 1..9 {
@@ -143,27 +128,33 @@ fn main() {
         for _i in 0..pw_len {
             password.push(0);
         }
-        /*
-        password[0] = 30;
-        password[1] = 39;
-        password[2] = 45;
-        password[3] = 30;
-        password[4] = 42;
-        */
 
         loop {
             //println!("password_nrs: {:?}", password);
-            let password_char_array = get_password_char_array(&password);
-            let result = try2decrypt(&data[start_of_data..start_of_data+zfr.compressed_size as usize], &password_char_array, zfr.c3c);
-            if result {
-                let password_string = get_password_string(&password);
-                println!("password found: {:?}", password_string);
-                return;
-            } else {
-                counter += 1;
-                if counter % 100000 == 0 {
+
+            if password.last().unwrap() % NUM_THREADS == thread_nr {
+                let password_char_array = get_password_char_array(&password);
+
+                let result = try2decrypt(&compressed_data, &password_char_array, crc);
+                if result {
                     let password_string = get_password_string(&password);
-                    println!("password wrong: {:?}", password_string);
+                    println!("password found: {:?}", password_string);
+                    print_current_time();
+                    let elapsed = NOW.elapsed();
+                    println!("Elapsed: {:.2?}", elapsed);
+                    process::exit(0);
+                } else {
+                    counter += 1;
+                    if counter % 100000 == 0 {
+                        let password_string = get_password_string(&password);
+                        println!("password wrong: {:?} {:?}", password_string, thread_nr);
+                    }
+                    if get_password_string(&password) == "ENTER" {
+                        let password_string = get_password_string(&password);
+                        println!("password wrong: {:?} {:?}", password_string, thread_nr);
+                    }
+                    //let password_string = get_password_string(&password);
+                    //println!("password wrong: {:?} {:?}", password_string, thread_nr);
                 }
             }
     
@@ -189,5 +180,89 @@ fn main() {
             }
         }
     }
+}
 
+fn print_current_time() {
+    let now = chrono::Local::now();
+    println!("{}", now.format("%b %-d, %-I:%M:%S").to_string());
+}
+
+fn main(){
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 1 {
+        println!("Usage: ZipMcKracken <zipfile>");
+        process::exit(1);
+    }
+
+    println!("ZipMcKracken running...");
+    print_current_time();
+    let elapsed = NOW.elapsed();
+    println!("Elapsed: {:.2?}", elapsed); // print once so we get the lazy init.
+    
+    let data = std::fs::read(&args[1]).unwrap();
+
+    // let s = std::mem::size_of::<ZipFileRecord>();
+    // println!("size_of ZipFileRecord: {}", s);
+    // wrong due to allignment issue
+
+    let zfr = ziputils::read_zip_file_record( &data );
+
+    ziputils::print_zip_file_record(&zfr);
+
+    let zip_file_record_size = 30;
+    let filename_end = zip_file_record_size + zfr.filename_length as usize;
+    let filename = &data[zip_file_record_size..filename_end];
+
+    let filename_str = std::str::from_utf8(&filename).unwrap();
+    println!("filename: {:?}", filename_str);
+
+    let extra = &data[filename_end..(filename_end + zfr.extra_field_length as usize)];
+
+    let start_of_data = filename_end + zfr.extra_field_length as usize;
+    
+    println!("extra: {:?}", extra);
+
+    let boxed_data = data.into_boxed_slice();
+    let boxed_data_static_ref = Box::leak(boxed_data);
+
+    let compressed_data = &boxed_data_static_ref[start_of_data..start_of_data+zfr.compressed_size as usize];
+
+    
+    let handles = (0..NUM_THREADS).map(|thread_nr| {
+        println!("HELLO1");
+        thread::spawn(move || {
+            main2(thread_nr, compressed_data, zfr.crc);
+        })
+        //println!("HELLO2");
+    }).collect::<Vec<_>>();
+    
+    for h in handles {
+        h.join().unwrap();
+    }
+
+    /*
+    let mut handles = Vec::new();
+    for thread_nr in 0..NUM_THREADS {
+        println!("HELLO1");
+
+        let res = thread::scope(|s| {
+            s.spawn(|_| {
+                main2(thread_nr, compressed_data, zfr.crc);
+            });
+        });
+        handles.push(res);
+        
+        println!("HELLO2");
+        
+    }
+
+    for thread_nr in 0..NUM_THREADS {
+        println!("HELLO3");
+        handles[thread_nr as usize].as_ref().unwrap();
+        println!("HELLO4");
+    }
+    */
+    
 }
--- src/ziputils.rs
+++ src/ziputils.rs
@@ -8,7 +8,7 @@ pub struct ZipFileRecord {
     pub compression: u16,
     pub time: u16,
     pub date: u16,
-    pub c3c: u32,
+    pub crc: u32,
     pub compressed_size: u32,
     pub uncompressed_size: u32,
     pub filename_length: u16,
@@ -24,7 +24,7 @@ pub fn read_zip_file_record(data:&[u8]) -> ZipFileRecord {
         compression: 0,
         time: 0,
         date: 0,
-        c3c: 0,
+        crc: 0,
         compressed_size: 0,
         uncompressed_size: 0,
         filename_length: 0,
@@ -34,14 +34,30 @@ pub fn read_zip_file_record(data:&[u8]) -> ZipFileRecord {
     let mut cursor = Cursor::new(data);
 
     zfr.signature = cursor.read_u32::<BigEndian>().unwrap();
+
+    assert_eq!(zfr.signature, 0x504b0304, "signature is not 0x504b0304");
+
     zfr.version = cursor.read_u16::<LittleEndian>().unwrap();
     zfr.flags = cursor.read_u16::<LittleEndian>().unwrap();
+
+    assert_eq!(zfr.flags, 1, "flags is not ZipCrypto");
+
+
     zfr.compression = cursor.read_u16::<LittleEndian>().unwrap();
+    
+    assert_eq!(zfr.compression, 8, "compression is not Deflate");
+
     zfr.time = cursor.read_u16::<LittleEndian>().unwrap();
     zfr.date = cursor.read_u16::<LittleEndian>().unwrap();
-    zfr.c3c = cursor.read_u32::<LittleEndian>().unwrap();
+    zfr.crc = cursor.read_u32::<LittleEndian>().unwrap();
     zfr.compressed_size = cursor.read_u32::<LittleEndian>().unwrap();
+
+    assert!( zfr.compressed_size > 0, "compressed_size is 0");
+
     zfr.uncompressed_size = cursor.read_u32::<LittleEndian>().unwrap();
+
+    assert!( zfr.uncompressed_size > 0, "uncompressed_size is 0");
+
     zfr.filename_length = cursor.read_u16::<LittleEndian>().unwrap();
     zfr.extra_field_length = cursor.read_u16::<LittleEndian>().unwrap();
 
@@ -56,7 +72,7 @@ pub fn print_zip_file_record(zfr : &ZipFileRecord) {
     println!("compression: {:X?}", zfr.compression);
     println!("time: {:X?}", zfr.time); // winstructs::timestamp::DosTime
     println!("date: {:X?}", zfr.date);
-    println!("c3c: {:X?}", zfr.c3c);
+    println!("c3c: {:X?}", zfr.crc);
     println!("compressed_size: {:?}", zfr.compressed_size);
     println!("uncompressed_size: {:?}", zfr.uncompressed_size);
     println!("filename_length: {:?}", zfr.filename_length);
